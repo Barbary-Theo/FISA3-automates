bd90ef6238f808fa5261799fb55b10361c20e9e2
Barbary-Theo <theo.barbary25@gmail.com>
2022-05-10 14:49:53 +0200

    start json reader merhod

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,25 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("oui");
     }
+
+
+
+    public static void readJSON(String filePath) throws FileNotFoundException {
+
+        filePath = "";
+
+        try {
+            new FileReader(filePath);
+        } catch (FileNotFoundException e) {
+            System.err.println("Error : File not found");
+        }
+
+
+    }
+
 }
