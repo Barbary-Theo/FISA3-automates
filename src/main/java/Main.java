1666b3428885bf3e77d66cc3aede80c1dec46615
Barbary-Theo <theo.barbary25@gmail.com>
2022-05-25 14:06:17 +0200

    contin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import model.Station;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.*;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -22,18 +22,18 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println(" \n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~> START <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
 
         Reseau reseau = Reseau.getInstance();
 
+        //boolean metroCheck = readMetroTEXT(new File("src/main/resources/metro.txt"), reseau);
+
+        /*
         boolean busCheck = readJSONBus("src/main/resources/bus.json", reseau);
-        //readMetroTEXT(new File("src/main/resources/metro.txt"));
         //checkTextFile(new File("/Users/martinthibaut/Desktop/metro.txt"));
         boolean trainCheck = readTrainXML("src/main/resources/train.xml", reseau);
         boolean tramCheck = readTramXML("src/main/resources/tram.xml", reseau);
@@ -42,9 +42,105 @@ public class Main {
             System.out.println(reseau);
             reseau.getCourtChemin("Gare", "0810", "Avlon");
         }
+         */
+
+
+        double start = 7;
+        double end = 22;
+        double finMatin = 9;
+        double intervalStartMatin = 10 / 100.0;
+        double intervalStartAprem = 20 / 100.0;
+
+        while (start <= end) {
+
+            System.out.println(start);
+
+            if(start < finMatin) start += intervalStartMatin;
+            else start += intervalStartAprem;
+            start = Math.round(start * 100.0) / 100.0;
+        }
+
+        checkHoraireValueAfterComputed(7.8);
+
     }
 
-    private static boolean readTramXML(String fileName, Reseau reseau) {
+
+    public static double checkHoraireValueAfterComputed(double value) {
+
+        int supValue = (int) Math.round(value);
+        int infValue;
+        double decimal;
+
+        if(supValue >= value) {
+            infValue = supValue - 1;
+            decimal = Math.abs(Math.round((infValue - value) * 100.0) / 100.0);
+        }
+        else {
+            infValue = supValue;
+            supValue += 1;
+            decimal = Math.abs(Math.round((infValue - value) * 100.0) / 100.0);
+        }
+
+        if(decimal >= 0.6) {
+
+        }
+
+        return supValue;
+
+    }
+
+
+    public static boolean readMetroTEXT(File fileName, Reseau reseau) throws Exception {
+
+        List<String> allLines = Files.readAllLines(Paths.get("src/main/resources/metro.txt"));
+
+        // 0) Devra récupérer les infos pour la création des trajets
+
+        for(int i = 0 ; i < allLines.size() ; i ++) {
+            int j = i;
+            String line = allLines.get(i);
+
+            if(line != null && line.contains("% depart arrivee duree")) {
+                j++;
+                List<String[]> circuitStocke = new ArrayList<>();
+
+                // 1) Récupérer les stations et leur durée
+                while (allLines.get(j) != null && !allLines.get(j).equals("") && !allLines.get(j).contains("%")) {
+                    String[] eleLine = allLines.get(j).split(" ");
+
+                    if (eleLine.length == 3) circuitStocke.add(eleLine);
+                    else {
+                        throwException("Error format -> line does not comport 'StationDepartName StationEndName Time' : " + allLines.get(j));
+                        return false;
+                    }
+                    j++;
+                }
+
+
+                // 2) Check conformité des éléments
+                for(int stringIndex = 0 ; stringIndex < circuitStocke.size() - 1 ; stringIndex ++) {
+                    if(!circuitStocke.get(stringIndex)[1].equals(circuitStocke.get(stringIndex + 1)[0])) {
+                        throwException("Error format -> end station is not the same than next line start station : " + Arrays.toString(circuitStocke.get(stringIndex)));
+                        return false;
+                    }
+                }
+
+
+                // 3) Création des trajets
+
+                i = j;
+
+                System.out.println();
+                break; // tempo
+            }
+
+        }
+
+        return true;
+    }
+
+
+    public static boolean readTramXML(String fileName, Reseau reseau) {
 
         boolean succes;
 
@@ -259,217 +355,6 @@ public class Main {
 
         }
         return true;
-    }
-
-
-    static void checkTextFile(File file) {
-        try {
-            List<String> allLines = Files.readAllLines(Paths.get("src/main/resources/metro.txt"));
-
-            // Récupération des données dans le fichier texte
-            int timeBreakStation = Integer.parseInt(allLines.get(allLines.size() - 5).substring(11, 12));
-            int timeStepHappyHour = Integer.parseInt(allLines.get(allLines.size() - 3).substring(28, 30));
-            int timeStepNoHappyHour = Integer.parseInt(allLines.get(allLines.size() - 2).substring(28, 30));
-
-            // Récupération des horaires du matin
-            StringBuilder sb = new StringBuilder(allLines.get(allLines.size() - 3).substring(42, 47));
-            sb.deleteCharAt(2);
-            String startLineTime1 = sb.toString();
-
-            sb = new StringBuilder(allLines.get(allLines.size() - 3).substring(50, 56));
-            sb.deleteCharAt(2);
-            String endLineTime1 = sb.toString();
-
-            // Récupération des horaires de l'après-midi
-            sb = new StringBuilder(allLines.get(allLines.size() - 3).substring(62, 67));
-            sb.deleteCharAt(2);
-            String startLineTime2 = sb.toString();
-
-            sb = new StringBuilder(allLines.get(allLines.size() - 3).substring(69, 74));
-            sb.deleteCharAt(2);
-            String endLineTime2 = sb.toString();
-
-            sb = new StringBuilder(allLines.get(allLines.size() - 1).substring(19, 24));
-            sb.deleteCharAt(2);
-            String endLineOfDay = sb.toString();
-
-            // Traitement sur le fichier texte
-            for (int i = 0; i < allLines.size(); i++) {
-                String line = allLines.get(i);
-
-                if (i == 0 && (!line.startsWith("%") || !line.contains("métro"))) {
-                    String error = "The first line must be begin with a %. (Line " + ++i + " in " + file.getName() + ")";
-                    throw new Exception(error);
-                }
-
-                if (i == 1 && (!line.startsWith("%") || !line.contains("stations"))) {
-                    String error = "The station tag was not found. (Line " + ++i + " in " + file.getName() + ")";
-                    try {
-                        throw new Exception(error);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                if (line.startsWith("%") && line.contains("stations")) {
-                    String temp = allLines.get(++i);
-                    String[] stations = temp.split(" ");
-                    Reseau reseau = Reseau.getInstance();
-                    for (String station: stations) {
-                        if (!reseau.verifStationExist(station)){
-                            String error = "The station \"" + station + "\" doesn't exist. (Line " + ++i + " in " + file.getName() + ")";
-                            try {
-                                throw new Exception(error);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    };
-                }
-
-                if (line.startsWith("%") && line.contains("liaisons A/R")) {
-                    try {
-                        Reseau reseau = Reseau.getInstance();
-                        int j = ++i;
-                        String temp = "";
-                        while (!(temp = allLines.get(j)).isEmpty()) {
-                            if (!temp.startsWith("%")) {
-                                System.out.println(temp);
-                            }
-                            j++;
-                        }
-                    }catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            }
-
-
-
-            /*BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-
-            String line;
-            int lineNumber = 1;
-            while ((line = bufferedReader.readLine()) != null) {
-                if (lineNumber == 1 && !line.startsWith("%") && !line.contains("métro")) {
-                    String error = "The first line must be begin with a %. (Line " + lineNumber + " in " + file.getName() + ")";
-                    throw new Exception(error);
-                }
-
-                Exploitant metro = new Exploitant("Métro");
-
-                // Checking stations in network
-                if (line.startsWith("%") && line.contains("stations")) {
-                    String temp = bufferedReader.readLine();
-                    lineNumber++;
-                    String[] stations = temp.split(" ");
-<<<<<<< HEAD
-                    int finalLineNumber = lineNumber;
-                    Reseau reseau = Reseau.getInstance();
-                    for (String station: stations) {
-                        if (!reseau.verifStationExist(station)){
-                            String error = "The station \"" + station + "\" doesn't exist. (Line " + finalLineNumber + " in " + file.getName() + ")";
-                            try {
-                                throw new Exception(error);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    };
-                }
-
-                // Adding links in network
-                if (line.startsWith("%") && line.contains("liaisons A/R") &&(bufferedReader.readLine().equals("% depart arrivee duree"))) {
-                    String temp = "";
-                    try {
-                        Reseau reseau = Reseau.getInstance();
-                        while (!(temp = bufferedReader.readLine()).startsWith("%") && !temp.isEmpty()) {
-                            String[] liaison = temp.split(" ");
-                            System.out.println(temp);
-                            Liaison l = new Liaison(setLiaisonName(liaison[0], liaison[1]), "0700", setTripTime(07, Integer.parseInt(liaison[2])), Integer.parseInt(liaison[2]), metro, reseau.findStationByName(liaison[0]), reseau.findStationByName(liaison[1]));
-                            System.out.println(l);
-
-                        }
-                        System.out.println("je sors");
-                    }catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-                }
-
-                // Adding circuit in network
-                if (line.startsWith("%") && line.contains("liaisons circuit") &&(bufferedReader.readLine().equals("% depart arrivee duree"))) {
-                    String temp = "";
-                    try {
-                        Reseau reseau = Reseau.getInstance();
-                        while (!(temp = bufferedReader.readLine()).startsWith("%") && !temp.isEmpty()) {
-                            String[] liaison = temp.split(" ");
-                            System.out.println(temp);
-                            Liaison l = new Liaison(setLiaisonName(liaison[0], liaison[1]), "0700", setTripTime(07, Integer.parseInt(liaison[2])), Integer.parseInt(liaison[2]), metro, reseau.findStationByName(liaison[0]), reseau.findStationByName(liaison[1]));
-                            System.out.println(l);
-
-                        }
-
-                    }catch (Exception e) {
-                        e.printStackTrace();
-                    }
-=======
-
->>>>>>> 1b2478f30f0b9873dadafbb30a63e2b2bf0d351e
-                }
-                lineNumber++;
-            }*/
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static String setStartTime(int timeBreakStation, int timeStepHappyHour, int timeStepNoHappyHour, String startTimeLine1, String endTimeLine1, String startTimeLine2, String endTimeLine2, String endLineOfDay, String name, int duree, Exploitant type, Station stationDepart, Station stationDestination) {
-        ArrayList<Liaison> liaisons = new ArrayList<Liaison>();
-        for (int i = 700; i <= 900; i+=10) {
-            if (i == 760) i = 800;
-            if (i == 860) i = 900;
-            liaisons.add(new Liaison(name, Integer.toString(i), setTripTime(i, duree), duree, type, stationDepart, stationDestination));
-        }
-
-        for (int i = 920; i <= 1630; i+=20) {
-            if (i == 960) i = 1000;
-            if (i == 1060) i = 1100;
-            if (i == 1160) i = 1200;
-            if (i == 1260) i = 1300;
-            if (i == 1360) i = 1400;
-            if (i == 1460) i = 1500;
-            if (i == 1560) i = 1600;
-            liaisons.add(new Liaison(name, Integer.toString(i), setTripTime(i, duree), duree, type, stationDepart, stationDestination));
-
-        }
-
-        for (int i = 1630; i <= 1800; i+=10) {
-            if (i == 1660) i = 1700;
-            if (i == 1760) i = 1800;
-            liaisons.add(new Liaison(name, Integer.toString(i), setTripTime(i, duree), duree, type, stationDepart, stationDestination));
-
-        }
-
-        for (int i = 1820; i <= 2300; i+=20) {
-            if (i == 1860) i = 1900;
-            if (i == 1960) i = 2000;
-            if (i == 2060) i = 2100;
-            if (i == 2160) i = 2200;
-            if (i == 2260) i = 2300;
-            liaisons.add(new Liaison(name, Integer.toString(i), setTripTime(i, duree), duree, type, stationDepart, stationDestination));
-
-        }
-        return liaisons.toString();
-    }
-
-    public static String setTripTime(int heure, int duree) {
-        return "" + (heure + duree);
-    }
-
-    public static String setLiaisonName(String depart, String arrive) {
-        return depart + arrive;
     }
 
 
